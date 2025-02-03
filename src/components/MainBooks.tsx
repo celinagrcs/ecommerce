8b312b8ac1e82341e1e37979e99a6e3884a19cdd
celinagrcs <celinagrcs@users.noreply.github.com>
2025-02-03 20:40:51 -0300

    add new transitions effects

--- src/components/MainBooks.tsx
+++ src/components/MainBooks.tsx
@@ -17,14 +17,14 @@ const MainBooks: React.FC = () => {
       </h2>
       <section className="flex flex-col sm:flex-row sm:flex-wrap justify-center items-center gap-6">
         {featuredBooks.map((book) => (
-          <article key={book.id} onClick={() => {handleClick(book.id)}} className="min-w-[300px] bg-white p-4 border border-[#ABD1C6] rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 flex-shrink-0 cursor-pointer">
+          <article key={book.id} onClick={() => {handleClick(book.id)}} className="min-w-[300px] bg-white p-4 border border-[#ABD1C6] rounded-lg shadow-lg hover:shadow-[#ABD1C6] transition-shadow duration-600 flex-shrink-0 cursor-pointer">
             <div className="flex justify-center items-center mb-4">  
               <img 
               src={book.image} alt={book.title} 
               className="object-contain h-48 w-48"
               />
             </div>
-            <h3 className="text-base font-bold mb-2 hover:text-[#078080]">
+            <h3 className="text-base font-bold mb-2 hover:text-[#078080] duration-300">
               <Link to={`/book/${book.id}`} onClick={(e) => e.stopPropagation()}>{book.title}</Link>
             </h3>
             <p className="text-gray-700">Autor: {book.author}</p>
